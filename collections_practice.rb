ae77d72712b5e561a88c07f49c42d1c3aaef2d7a
Thomas Edwards <thce28@gmail.com>
2019-02-14 17:01:04 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,46 @@
-# your code goes here
+require 'pry'
+
+def begins_with_r(arr)
+  new_arr = []
+  arr.each do |x|
+  new_arr = x[0] == "r"
+
+  end  
+  return new_arr
+end
+
+
+def contain_a(array)
+  arr = []
+  array.each do |i|
+    if i.include?("a")
+      arr << i  
+
+    end   
+  end  
+  arr
+end
+
+def first_wa(array)
+x = ""
+  array.find do |i|
+  if i[0..1] == 'wa'
+    return i  
+
+  end 
+  end 
+end
+
+
+def remove_non_strings(array)
+
+  array.delete_if { |obj|  !(obj.is_a? String)}
+
+end
+
+def count_elements(array)
+  
+  
+end
+
+
